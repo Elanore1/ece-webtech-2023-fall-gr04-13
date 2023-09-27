f695e83763c627775782d8aff8105c4dd6c6f354
noepham <noe.pham@edu.ece.fr>
2023-09-27 10:19:59 +0200

    feat:express

---
+++ routes/user.js
@@ -0,0 +1,23 @@
+
+/*
+const express = require('express');
+const router = express.Router();
+
+
+// Route handler for getting all users
+router.get('/', (req, res) => {
+    // Your logic here
+  });
+  
+  // Route handler for getting a specific user by ID
+  router.get('/:id', (req, res) => {
+    // Your logic here
+  });
+  
+  // Other route handlers for creating, updating, and deleting users
+  
+  module.exports = router;
+// Define user routes here
+
+module.exports = router;
+*/
