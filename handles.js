948fa32ec6a167051f49e4a3ecf3351160363a54
solveigus <solveig.berling@edu.ece.fr>
2023-09-27 09:48:18 +0200

    feat: express

--- handles.js
+++ handles.js
@@ -1,77 +1,68 @@
+const express = require('express');
+const app = express();
+
 // Necessary imports
-// Import Node url module
-const url = require('url')
-const qs = require('querystring')
+const url = require('url');
+const qs = require('querystring');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
-    serverHandle: function (req, res) {
-        const route = url.parse(req.url)
-        const pathname = route.pathname 
-        const params = qs.parse(route.query)
-        
-        //Creating an application with 3 routes
-        if(pathname == '/'){
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            //explaining how /hello works
-            const explanation = `
-            <h1>Explanation:</h1>
 
-            <p><a href="John">John</a> - Replies with "Hello, John!"</p>
-            <p><a href="Elanore">Elanore</a> - Replies with a short intro of Elanore</p>
-            <p><a href="Solveig">Solveig</a> - Replies with a short intro of Solveig</p>
-            <p><a href="Noe">Noe</a> - Replies with a short intro of Noe</p>
+// Define a route for the root path
+app.get('/', (req, res) => {
+  res.status(200).send(`
+    <h1>Explanation:</h1>
+    <p><a href="/hello?name=John">John</a> - Replies with "Hello, John!"</p>
+    <p><a href="/hello?name=Elanore">Elanore</a> - Replies with a short intro of Elanore</p>
+    <p><a href="/hello?name=Noe">/hello?name=Noe</a> - Replies with a short intro of Noe</p>
+    <p><a href="/hello?name=Solveig">Solveig</a> - Replies with a short intro of Solveig</p>
+    <p><a href="/test">test</a> - Any other path replies with a 404 code and a "Not Found" message.</p>
+  `);
+});
 
-            <p><a href="test">test</a> - Any other path replies with a 404 code and a "Not Found" message.</p>
-            `;
-            res.end(explanation);
-        }else if (pathname === '/hello' && 'name' in params) {
-            
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            if(params['name']=='Elanore'){
-                //My name => hello + short itroduction
-                res.write('Hello Guys, I am Elanore Lelievre, a 4th year student at the ECE school ') 
-                res.write('<p>I am 20 and I love node.js<p>') 
-            }else if(params['name']=='Noe'){
-                res.write('Hello , my name is Noe , I am 20 years old , i like playing video games and watching movies!')
-            }else if(params['name']=='Solveig'){
-            res.write('I am Solveig Berling, also a 4th year student at ECE') 
-            res.write('<p>I am 20 and I am fond WebTech<p>')
-            }else{
-                //Just saying hello name
-               res.write('Hello ' + params['name']) 
-            }
-            res.end();
-        }else if(pathname == '/about'){
-            // Construct the path to the about.json file
-            const aboutPath = 'content/about.json';
+// Define a route for the /hello path
+app.get('/hello', (req, res) => {
+  const name = req.query.name;
 
-            // Check if the about.json file exists
-            fs.access(aboutPath, fs.constants.F_OK, (err) => {
-                if (err) {
-                 res.sendStatus(404);
-                // res.write('Not Found');
-                    res.end();
-                } else {
-                    // If the file exists, read and send its content as JSON
-                    fs.readFile(aboutPath, 'utf8', (err, data) => {
-                        if (err) {
-                            res.writeHead(500, { 'Content-Type': 'text/plain' });
-                            res.write('Internal Server Error');
-                        } else {
-                            res.writeHead(200, { 'Content-Type': 'application/json' });
-                            res.write(data);
-                        }
-                        res.end();
-                    });
-                }
-            });
-        }else {
-            //EREOR MESSAGE
-            res.sendStatus(404);
-            res.end();
-        }
-        
-    } 
+  if (name === 'Elanore') {
+    res.status(200).send('Hello Guys, I am Elanore Lelievre, a 4th year student at the ECE school\nI am 20 and I love node.js');
+  }else if(params['name']=='Noe'){
+    res.write('Hello , my name is Noe , I am 20 years old , i like playing video games and watching movies!')
+  }else if (name === 'Solveig') {
+    res.status(200).send('I am Solveig Berling, also a 4th year student at ECE\nI am 20 and I am fond of WebTech');
+  } else {
+    res.status(200).send(`Hello ${name}`);
   }
+});
+
+// Define a route for the /about path
+app.get('/about', (req, res) => {
+  // Construct the path to the about.json file
+  const aboutPath = 'content/about.json';
+
+  // Check if the about.json file exists
+  fs.access(aboutPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      // If the file doesn't exist
+      res.status(404).send('Not Found');
+    } else {
+      // If the file exists, read and send its content as JSON
+      fs.readFile(aboutPath, 'utf8', (err, data) => {
+        if (err) {
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.status(200).json(JSON.parse(data));
+        }
+      });
+    }
+  });
+});
+
+// Handle all other routes with a 404 response
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+module.exports = {
+  serverHandle: app,
+};
