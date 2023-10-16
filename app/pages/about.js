e082e77641de97bf5912ff20d32e452a644dafaa
Elanore1 <elanore.lelievre@edu.ece.fr>
2023-10-16 19:23:09 +0200

    feat: pages index, articles, about and hello

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,23 +1,24 @@
 import Head from 'next/head'
 import Layout from '../components/Layout.js'
+import Link from 'next/link';
+import styles from '../styles/Layout.module.css'
 
 export default function Page() {
   return (
     <Layout>
       <Head>
-        <title>WebTech - About us</title>
+        <title>MyStore - About us</title>
         <meta name="description" content="Don't be shy, drop us an email" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>
-        About us
-      </h1>
-      <p><a href="/hello?name=John">test</a></p>
-    <p><a href="/hello?name=Elanore">Elanore</a></p>
-    <p><a href="/hello?name=Noe">Noé</a></p>
-    <p><a href="/hello?name=Solveig">Solveig</a></p>
-    <p><a href="/hello?test">Error</a></p>
-
+      <div className={styles.centeredText}>
+        <h1> About us </h1>
+        <p><a href="/hello?name=John">Testing with a general name</a></p>
+        <p><a href="/hello?name=Elanore">Elanore</a></p>
+        <p><a href="/hello?name=Noe">Noé</a></p>
+        <p><a href="/hello?name=Solveig">Solveig</a></p>
+        <p><a href="/hello?test">Error - No Name Provided</a></p>
+      </div>
     </Layout>
   ) 
 }
