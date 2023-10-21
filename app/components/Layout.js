c25f7db85a67bc0a1e3498c5da808cbcbaadeda8
Elanore1 <elanore.lelievre@edu.ece.fr>
2023-10-21 13:36:01 +0200

    feat: use SSG by implementing geStaticProps

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,15 +1,27 @@
-import Header from './Header.js'
+
+import Head from 'next/head'
+import Header from '../components/Header.js'
 import Footer from '../components/Footer.js'
-//container div 
-//main style main
-export default function Layout({children}){
+
+export default function Layout({
+  children,
+  title,
+  description
+}){
   return (
-    <div className="p-0">
-      <Header/>
-      <main className="min-h-screen p-16 md:p-16 lg:p-16 bg-whiteSpecial">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Webtech - {title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="p-0">
+        <Header />
+        <main className="min-h-screen p-16 md:p-16 lg:p-16 bg-whiteSpecial">
+          {children}
+        </main>
+        <Footer />
+      </div>
+    </>
   )
 }
