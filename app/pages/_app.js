a10b10ca4ec15da172764c5cf3ce3d25c38752d4
Elanore1 <elanore.lelievre@edu.ece.fr>
2023-11-13 13:05:05 +0100

    feat: fetch data from supabase

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,10 +1,23 @@
 import '@/styles/globals.css'
 import { UserProvider } from '@/components/UserContext'
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 export default function App({ Component, pageProps }) {
+  // Create a new supabase browser client on every first render.
+  const [supabaseClient] = useState(() => createPagesBrowserClient({ supabaseUrl, supabaseAnonKey }))
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    > 
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </SessionContextProvider>
   )
 }
